3cb1c7e1044f3feda31c947238f2025079d5a0e9
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-28 10:58:38 +0200

    feat: costruttori e primi due metodi

--- src/main/java/org/iterations/java/Iterables.java
+++ src/main/java/org/iterations/java/Iterables.java
@@ -1,5 +1,27 @@
 package org.iterations.java;
 
 public class Iterables {
+	
+	int[] numbers;
+	int index;
+	
+	public Iterables(int[] numbers) {
+		this.numbers = numbers;
+	}
+	
+	public Iterables() {
+		this.numbers = new int[0];
+	}
 
+	public int getElementoSuccessivo() {
+		int storedValue = numbers[index];
+        if (this.index < numbers.length) this.index++; 
+		return storedValue;
+	}
+	
+	public boolean hasAncoraElementi() {
+		return (this.index < numbers.length);
+	}
+	
+	
 }
