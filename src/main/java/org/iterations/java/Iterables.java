123743cb1de630925062ee4166c4e92cb29fba31
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-28 12:02:06 +0200

    feat: javadoc

--- src/main/java/org/iterations/java/Iterables.java
+++ src/main/java/org/iterations/java/Iterables.java
@@ -5,16 +5,27 @@ public class Iterables {
 	private int[] numbers;
 	private int index;
 	
-	// costruttori
+	/**
+	 * Costruttore con parametro
+	 * 
+	 * @param array di interi
+	 */
 	public Iterables(int[] numbers) {
 		this.numbers = numbers;
 	}
 	
+	/**
+	 * Costruttore senza parametro che inizializza l'array come vuoto
+	 */
 	public Iterables() {
 		this.numbers = new int[0];
 	}
  
-	// metodi principali
+	/**
+	 * Metodo che restituisce l'elemento successivo
+	 * 
+	 * @return nuovo intero
+	 */
 	public int getElementoSuccessivo() {
         int value = 0;
         if (this.index < numbers.length) {
@@ -24,12 +35,17 @@ public class Iterables {
 		return value;
 	}
 	
+	/**
+	 * Metodo che indica se abbiam stampato tutti gli elementi o meno
+	 * 
+	 * @return true/false
+	 */
 	public boolean hasAncoraElementi() {
 		return (this.index < numbers.length);
 	}
 	
 	/**
-	 * Metodo Bonus
+	 * Metodo Bonus per l'aggiunta di un elemento
 	 * 
 	 * @param newNumber
 	 */
